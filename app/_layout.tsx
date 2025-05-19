09a2f46df7f6349e18f5656de88126c2c2dec307
Abdiel <abdimuy@gmail.com>
2025-05-19 12:47:17 -0600

    project setup

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Slot } from 'expo-router';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Slot />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Hello, this is the home screen!</Text>
+    </View>
+  );
+};
+
+export default Home;
