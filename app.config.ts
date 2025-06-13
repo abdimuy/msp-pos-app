c48278f5ce6edd7dd3f64d17a4b5520bbd8904f2
Abdiel <abdimuy@gmail.com>
2025-06-13 10:48:07 -0600

    add expo build config

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,12 @@
-import { ExpoConfig, ConfigContext } from '@expo/config'
+import { ExpoConfig, ConfigContext } from '@expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'sistemaCobroAppMsp',
   slug: 'sistemaCobroAppMsp',
+  android: {
+    package: 'com.abdidev.sistemacobro',
+  },
   extra: {
     firebase: {
       apiKey: process.env.FIREBASE_API_KEY,
@@ -12,6 +15,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
+    },
+    eas: {
+      projectId: '7ae6d062-cdbb-4020-9e27-56019ad2c580',
     },
   },
 });
